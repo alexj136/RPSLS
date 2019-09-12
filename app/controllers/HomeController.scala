26d745b04619426ade3c010d701ec3760aea5e9e
Alex Jeffery <apjeffery136@googlemail.com>
2019-09-12 20:05:35 +0100

    removed some cruft

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -21,13 +21,4 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
-  
-  def explore() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.explore())
-  }
-  
-  def tutorial() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.tutorial())
-  }
-  
 }
